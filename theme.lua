47ba21d7171f3d86992b6376ec25b631b5788fb7
Bartek Gładecki <bgladecki@gmail.com>
2021-05-05 12:20:30 +0200

    layouts theme mods

--- theme.lua
+++ theme.lua
@@ -4,6 +4,9 @@
 local awful = require("awful")
 awful.util = require("awful.util")
 
+local gfs = require("gears.filesystem")
+local default_theme_path = gfs.get_themes_dir() .. "/default/"
+
 theme = {}
 
 --}}}
@@ -42,8 +45,24 @@ theme.gap_single_client = true
 theme.useless_gap = 5
 
 -- Icons
-
 theme.icon_theme = 'Papirus-Dark'
+
+theme.layout_fairh      = default_theme_path.."layouts/fairhw.png"
+theme.layout_fairv      = default_theme_path.."layouts/fairvw.png"
+theme.layout_floating   = default_theme_path.."layouts/floatingw.png"
+theme.layout_magnifier  = default_theme_path.."layouts/magnifierw.png"
+theme.layout_max        = default_theme_path.."layouts/maxw.png"
+theme.layout_fullscreen = default_theme_path.."layouts/fullscreenw.png"
+theme.layout_tilebottom = default_theme_path.."layouts/tilebottomw.png"
+theme.layout_tileleft   = default_theme_path.."layouts/tileleftw.png"
+theme.layout_tile       = default_theme_path.."layouts/tilew.png"
+theme.layout_tiletop    = default_theme_path.."layouts/tiletopw.png"
+theme.layout_spiral     = default_theme_path.."layouts/spiralw.png"
+theme.layout_dwindle    = default_theme_path.."layouts/dwindlew.png"
+theme.layout_cornernw   = default_theme_path.."layouts/cornernww.png"
+theme.layout_cornerne   = default_theme_path.."layouts/cornernew.png"
+theme.layout_cornersw   = default_theme_path.."layouts/cornersww.png"
+theme.layout_cornerse   = default_theme_path.."layouts/cornersew.png"
 
 -- Default master width
 theme.master_width_factor = 0.7
