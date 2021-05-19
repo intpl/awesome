09a675b165dd19c9fd04cfdf395518ac1fefa56f
Bartek Gładecki <bgladecki@gmail.com>
2021-05-19 08:54:59 +0200

    move autostart to separate file

---
+++ autostart.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run "blueman-applet"
+# run "nitrogen --restore"
+run "compton"
+run "cbatticon"
+run "pasystray"
+run "nm-applet"
+run "xfce4-volumed"
+run "dropbox start"
