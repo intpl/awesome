10fc30c01c4b44e09973c50edd3d0e7d10217777
Bartek Gładecki <bgladecki@gmail.com>
2021-09-19 10:02:33 +0200

    maximize on right mouse button

--- rc.lua
+++ rc.lua
@@ -337,9 +337,7 @@ local tasklist_buttons = gears.table.join(
                                                   )
                                               end
                                           end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
+                     awful.button({ }, 3, mymaximize), -- awful.menu.client_list({ theme = { width = 250 } })
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
                                           end),
