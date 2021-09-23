8c79de94657a80834771f6a760d7e860c86fed0e
Bartek Gładecki <bgladecki@gmail.com>
2021-09-23 20:57:23 +0200

    gap bigger after all

--- theme.lua
+++ theme.lua
@@ -49,7 +49,7 @@ theme.tasklist_disable_icon = true
 
 -- Gaps
 theme.gap_single_client = true
-theme.useless_gap = 2
+theme.useless_gap = 20
 
 -- Maximized clients have no border
 theme.maximized_hide_border = true
