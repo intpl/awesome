89d4f12fc0611e4686662828fd936f79a0668b0a
Bartek Gładecki <bgladecki@gmail.com>
2021-09-26 21:43:17 +0200

    use bling window switcher (untested on dual screen)

--- rc.lua
+++ rc.lua
@@ -215,6 +215,19 @@ beautiful.init("~/.config/awesome/theme.lua")
 local bling = require("bling")
 bling.module.flash_focus.enable()
 
+bling.widget.window_switcher.enable {
+    type = "thumbnail", -- set to anything other than "thumbnail" to disable client previews
+
+    hide_window_switcher_key = "Escape", -- The key on which to close the popup
+    minimize_key = "n",     -- The key on which to minimize the selected client
+    unminimize_key = "N",   -- The key on which to unminimize all clients
+    kill_client_key = "q",  -- The key on which to close the selected client
+    cycle_key = "Tab",      -- The key on which to cycle through all clients
+    previous_key = "Left",  -- The key on which to select the previous client
+    next_key = "Right",     -- The key on which to select the next client
+    vim_previous_key = "h", -- Alternative key on which to select the previous client
+    vim_next_key = "l",     -- Alternative key on which to select the next client
+}
 
 terminal = "alacritty"
 terminal_with_tmux = terminal .. " -e /usr/bin/env tmux"
@@ -515,22 +528,8 @@ globalkeys = gears.table.join(
     awful.key({ modkey,           }, "y", awful.tag.togglemfpol,
               {description = "toggle master size fill policy", group = "client"}),
 --    cyclefocus.key({ modkey_alt, }, "Tab", {}),
-    -- TODO: use keygrabber
     awful.key({ modkey_alt,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end),
-
-    awful.key({ modkey_alt, "Shift"   }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end),
+        function() awesome.emit_signal("bling::window_switcher::turn_on") end),
 
     -- Standard program
     awful.key({ modkey, "Shift" }, "r", awesome.restart,
@@ -993,6 +992,7 @@ end)
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
+  -- FIXME this may be responsible for hangs
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
