732707e0cf8b90b0f31df88162733d0b2a839298
Bartek Gładecki <bgladecki@gmail.com>
2021-09-27 23:34:20 +0200

    autostart cleanup
    pgrep gets angry when gets two arguments

--- rc.lua
+++ rc.lua
@@ -1044,7 +1044,6 @@ awful.spawn.with_shell("nitrogen --restore")
 -- Autorun/autostart programs
 awful.spawn.with_shell("killall light-locker; light-locker --lock-on-lid --lock-on-suspend --no-late-locking") -- slock is introducing errors?
 awful.spawn.with_shell("dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY")  -- gtk apps take ages to load without that
--- awful.spawn.with_shell("killall ulauncher; ulauncher --no-window-shadow --hide-window")
 awful.spawn.with_shell("killall conky ; conky")
-
+awful.spawn.with_shell("dropbox start") -- will not interfere if it's already running
 awful.spawn.with_shell("~/.config/awesome/autostart.sh")
