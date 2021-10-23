edd177a9b302301a1b37a80f4ffdef10f569a41c
Bartek Gładecki <bgladecki@gmail.com>
2021-10-23 22:03:35 +0200

    change leader from ; to ,

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+c.editor.command = ['alacritty', '-e', 'nvim', '{file}']
+
 config.bind('d', 'scroll-page 0 0.5')
 config.bind('u', 'scroll-page 0 -0.5')
 config.bind('x', 'tab-close')
-config.bind(';u', 'undo')
-config.bind(';U', 'undo -w')
-config.bind(';xo', 'set-cmd-text -s :open -b')
-config.bind(';xO', 'set-cmd-text :open -b -r {url:pretty}')
-
-c.editor.command = ['alacritty', '-e', 'nvim', '{file}']
+config.bind(',u', 'undo')
+config.bind(',U', 'undo -w')
+config.bind(',xo', 'set-cmd-text -s :open -b')
+config.bind(',xO', 'set-cmd-text :open -b -r {url:pretty}')
+config.bind(',e', 'edit-url')
