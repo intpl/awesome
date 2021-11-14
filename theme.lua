282a5ccedb0878d4bfbf6e55f800f8f30af8893b
intpl <intpl@protonmail.com>
2021-11-14 17:45:37 +0100

    smaller font

--- theme.lua
+++ theme.lua
@@ -9,7 +9,7 @@ local default_titlebar_path = default_theme_path .. "/titlebar/"
 -- Theme fonts and colors
 theme = {}
 
-theme.font                     = "Iosevka Term SS09 14"
+theme.font                     = "Iosevka Term SS09 11"
 theme.hotkeys_font             = "Iosevka Term SS09 17"
 theme.hotkeys_description_font = "Iosevka Term SS09 17"
 
