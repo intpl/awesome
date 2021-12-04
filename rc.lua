fcea1a0b81d4a4cba81d3885483db19b6856c181
intpl <intpl@protonmail.com>
2021-12-04 10:48:55 +0100

    different layout order

--- rc.lua
+++ rc.lua
@@ -249,8 +249,8 @@ modkey_alt = "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
