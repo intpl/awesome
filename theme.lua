b81fdd8f71d4211215bfd527fa0375b26b08b75e
bartek <bartek@reallytrusted.com>
2022-02-16 17:45:20 +0000

    let's hide borders, pretend transparency is enough

--- theme.lua
+++ theme.lua
@@ -39,7 +39,7 @@ theme.tasklist_fg_normal     = "#666666"
 theme.tasklist_fg_focus     = "#ffffff"
 theme.tasklist_bg_focus     = "#404040aa"
 
-theme.border_width  = 1
+theme.border_width  = 0
 theme.border_normal = "#222222"
 theme.border_focus  = "#444444"
 theme.border_marked = "#91231c"
