e2a2f11b66a87ac55425cb725fbff0da563770f1
bartek <bartek@reallytrusted.com>
2022-06-05 16:51:35 +0100

    change toggle tag kbdgs to win+alt

--- rc.lua
+++ rc.lua
@@ -933,17 +933,17 @@ for i = 1, 9 do
                      end
                   end,
                   {description = "move focused client to tag #"..i, group = "tag"}),
-                -- Toggle tag on focused client.
-                awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                    if client.focus then
-                      local tag = client.focus.screen.tags[i]
-                      if tag then
+        -- Toggle tag on focused client.
+        awful.key({ modkey, modkey_alt }, "#" .. i + 9,
+            function ()
+                if client.focus then
+                    local tag = client.focus.screen.tags[i]
+                    if tag then
                         client.focus:toggle_tag(tag)
-                      end
                     end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
+                end
+            end,
+            {description = "toggle focused client on tag #" .. i, group = "tag"})
     )
 end
 
